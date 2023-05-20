12fbeb85dd5dafc85e7b6e3b9952209125f3fc4e
Eva Niehaus <eniehaus111@gmail.com>
2023-05-20 22:13:09 +0200

    add flux logic

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -9,15 +9,19 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     const addPost = () => {
+        props.addPost();
+    };
+
+    let onPostChange = () => {
         let text = newPostElement.current.value;
-        alert(text);
+        props.updateNewPostText(text);
     };
 
     return (<div className = { s.postsBlock }>
         <h3>My posts</h3>
         <div>
             <div>
-                <textarea ref = { newPostElement }></textarea>
+                <textarea ref = { newPostElement } onChange = { onPostChange } value = { props.newPostText } />
             </div>
             <div>
                 <button onClick = { addPost }>Add post</button>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,29 +1,49 @@
+import { rerenderEntireTree } from '../render';
+
 let state = {
     profilePage: {
         posts: [
             { id: 1, message: 'Hi, how are you?', likesCount: 15 },
             {
-            id: 2, message: "It's my first post", likesCount: 20
-        },
+                id: 2, message: "It's my first post", likesCount: 20
+            },
             { id: 3, message: "Hi there!", likesCount: 20 },
             { id: 4, message: "Yo!", likesCount: 20 },
-        ]
+        ],
+        newPostText: 'it-kamasutra.com'
     },
+
     dialogsPage: {
         dialogs: [
             { id: 1, name: 'Evchen' },
             { id: 2, name: 'Adam' }, { id: 3, name: 'Kain' },
-            { id: 4, name: 'Abel'},
+            { id: 4, name: 'Abel' },
             { id: 5, name: 'Lea' },
         ],
-        messages:   [
+        messages: [
             { id: 1, message: 'Hi!' },
             { id: 2, message: 'How is your day?' },
-            { id: 3, message: 'Yo!'},
+            { id: 3, message: 'Yo!' },
             { id: 4, message: 'Well!' },
             { id: 5, message: 'Fine!' }
         ]
     }
+};
+
+export let addPost = () => {
+    let newPost = {
+        id: 5,
+        message: state.profilePage.newPostText,
+        likesCount: 0
+    };
+    state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+};
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
 };
 
 export default state;
