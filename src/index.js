b2e8a742c5b695963a16f7afed72222419edc7dc
Eva Niehaus <eniehaus111@gmail.com>
2023-05-21 23:39:22 +0200

    add observer pattern

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
-import state from './redux/state';
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { addPost, updateNewPostText } from './redux/state';
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let rerenderEntireTree = (state) => {
+    root.render(
+        <BrowserRouter>
+            <App state = { state } addPost = { addPost } updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>
+    );
+};
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
@@ -13,3 +22,4 @@ import state from './redux/state';
 reportWebVitals();
 
 rerenderEntireTree(state)
+subscribe(rerenderEntireTree)
