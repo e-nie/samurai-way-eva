63a105f1078ac1fff3c939733be0ff860a810bfc
Eva Niehaus <eniehaus111@gmail.com>
2023-06-06 00:49:05 +0200

    add Context

--- src/components/Dialogs/DialogsContainer.js
+++ src/components/Dialogs/DialogsContainer.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import s from './Dialogs.module.css';
-import DialogItem from './DialogItem/DialogsItem';
-import Message from './Message/Message';
 import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialogs-reducer';
 import Dialogs from './Dialogs';
+import StoreContext from '../../StoreContext';
 
+const DialogsContainer = () => {
 
-const DialogsContainer = (props) => {
-    let state = props.store.getState().dialogsPage;
+    return <StoreContext.Consumer>
+        { (store) => {
 
-    const onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
-    };
+        const onSendMessageClick = () => {
+            store.dispatch(sendMessageCreator());
+        };
 
-    const onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    };
-
-    return <Dialogs updateNewMessageBody = { onNewMessageChange } sendMessage={onSendMessageClick}
-    dialogsPage={state}
-    />;
+        const onNewMessageChange = (body) => {
+            store.dispatch(updateNewMessageBodyCreator(body));
+        };
+        return <Dialogs updateNewMessageBody = { onNewMessageChange }
+                        sendMessage = { onSendMessageClick }
+                        dialogsPage = { store.getState().dialogsPage } />;
+    } }
+    </StoreContext.Consumer>;
 };
 
 export default DialogsContainer;
