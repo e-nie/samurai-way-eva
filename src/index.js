284a4d4b5636013fc044eb6f761142220cd942d6
Eva Niehaus <eniehaus111@gmail.com>
2023-06-08 02:03:50 +0200

    create state copy

--- src/index.js
+++ src/index.js
@@ -7,21 +7,16 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 
-let rerenderEntireTree = (state) => {
+
     ReactDOM.render(
         <BrowserRouter>
             <Provider store = { store }>
                 <App />
             </Provider>
-        </BrowserRouter>, document.getElementById('root'));
-};
+        </BrowserRouter>, document.getElementById('root'))
 
-rerenderEntireTree(store.getState());
 
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
-});
+
 
 
 // API
