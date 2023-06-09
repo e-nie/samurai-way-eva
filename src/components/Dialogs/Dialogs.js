f74664e45a2627a05283eb7832037df05f266c31
Eva Niehaus <eniehaus111@gmail.com>
2023-06-10 01:44:03 +0200

    refactor code

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -7,8 +7,8 @@ import Message from './Message/Message';
 const Dialogs = (props) => {
     let state = props.dialogsPage;
 
-    let dialogsElements = state.dialogs.map(d => <DialogItem name = { d.name } id = { d.id } />);
-    let messagesElements = state.messages.map(m => <Message id = { m.id } message = { m.message } />);
+    let dialogsElements = state.dialogs.map(d => <DialogItem name = { d.name } key={d.id} id = { d.id } />);
+    let messagesElements = state.messages.map(m => <Message id = { m.id } key={m.id} message = { m.message } />);
     let newMessageBody = state.newMessageBody;
 
 
